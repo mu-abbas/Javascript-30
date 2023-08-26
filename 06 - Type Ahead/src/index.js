70d098d5c2109ce503e023285c09b06e8ec8b838
Muhammad <md.devjs@gmail.com>
2023-08-26 22:46:48 +0300

    finish type ahead challenge

---
+++ 06 - Type Ahead/src/index.js
@@ -0,0 +1,64 @@
+const search = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+const cities = [];
+
+const url =
+  'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+fetch(url)
+  .then(res => res.json())
+  .then(data => cities.push(...data));
+
+console.log(cities);
+
+function searchResults(word, data) {
+  return data.filter(
+    place =>
+      place.city.toLowerCase().includes(word.toLowerCase()) || place.state.toLowerCase().includes(word.toLowerCase())
+  );
+}
+
+function displayResults() {
+  const word = this.value;
+  if (word == '') {
+    suggestions.innerHTML = `
+          <li class="suggestions-item">
+            <p>filter for a city</p>
+          </li>
+          <li class="suggestions-item">
+            <p>or a state</p>
+          </li>
+    `;
+    return;
+  }
+  const data = searchResults(word, cities);
+  let cityName;
+  let stateName;
+  let population;
+  const html = `
+  ${data
+    .map(place => {
+      if (place.city.toLowerCase().includes(word.toLowerCase())) {
+        cityName = place.city.toLowerCase().replace(word.toLowerCase(), `<span class='hl'>${word}</span>`);
+      }
+      if (place.state.toLowerCase().includes(word.toLowerCase())) {
+        stateName = place.state.toLowerCase().replace(word.toLowerCase(), `<span class='hl'>${word}</span>`);
+      }
+      population = +place.population;
+      console.log(population);
+      return `
+        <li class="suggestions-item">
+          <p>${cityName}, ${stateName}</p>
+          <p>${population.toLocaleString('en-US')}</p>
+        </li>
+    `;
+    })
+    .join('')}
+  `;
+
+  suggestions.innerHTML = html;
+}
+
+search.addEventListener('change', displayResults);
+search.addEventListener('keyup', displayResults);
