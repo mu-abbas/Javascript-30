4dc456518dfd778542aebe4400e4e1cfa613615f
Muhammad <md.devjs@gmail.com>
2023-08-27 21:56:45 +0300

    explore some console methods

---
+++ 09 - Dev Tools Domination/index.js
@@ -0,0 +1,22 @@
+// console for developping
+
+console.log();
+//show something in browser console
+console.warn();
+//show something in browser console in warning block (yellow colored)
+console.error();
+//show something in browser console in error block (red color)
+console.clear();
+// clear browser console
+console.assert();
+// for testing if true or false
+console.dir();
+// to view dom elements
+console.group();
+// to group multiple console logs followed by console.groupEnd();
+console.time();
+// to show the time consumed to run a specific function followed by console.timeEnd();
+console.info();
+//show something in browser console with info symbol
+console.table();
+// show array elements in table in the console
