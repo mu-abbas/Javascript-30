48c9c7cfcbf35cca7249051673de27db81da89e4
Muhammad <md.devjs@gmail.com>
2023-09-09 15:50:36 +0300

    finish 17th challenge

---
+++ 17 - Sort Without Articles/index.js
@@ -0,0 +1,23 @@
+const bands = [
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog',
+];
+
+function trim(string) {
+  return string.replace(/the|an|a/gi, '').trim();
+}
+
+const sortedBands = bands.sort((a, b) => (trim(a) > trim(b) ? 1 : -1));
+
+console.log(sortedBands);
